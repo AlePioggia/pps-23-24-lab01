955076f43e615746eb6ebe5229d6f8077265bd2a
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2024-02-26 16:13:38 +0100

    implement forward iterator

--- tdd-exercise/src/main/java/tdd/alternative/CircularListImpl.java
+++ tdd-exercise/src/main/java/tdd/alternative/CircularListImpl.java
@@ -1,19 +1,43 @@
 package tdd.alternative;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 
 public class CircularListImpl implements CircularList {
 
+    private List<Integer> list = new ArrayList<>();
+    private Iterator<Integer> iterator;
+
+    public CircularListImpl() {
+        this.iterator = this.list.iterator();
+    }
+
     @Override
     public void add(int element) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        this.list.add(element);
+        this.iterator = this.list.iterator();
     }
 
     @Override
     public Iterator<Integer> forwardIterator() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'forwardIterator'");
+        return new Iterator<Integer>() {
+
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+
+            @Override
+            public Integer next() {
+                if (!iterator.hasNext()) {
+                    iterator = list.listIterator();
+                }
+                return iterator.next();
+            }
+
+        };
     }
 
 }
